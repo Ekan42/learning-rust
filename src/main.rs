f032379d8662548dad1a3b92624fe7dd5ab5f8a5
Ekan42 <153546894+Ekan42@users.noreply.github.com>
2023-12-13 21:16:49 +0100

    Newer code from the book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn times_two(number: i32) -> i32 {
+    number * 2
+}
 fn main() {
-    println!("Hello, world!");
+    let final_number = {
+    let y = 10;
+    let x = 9; 
+    let x = times_two(x);
+    let x = x + y;
+    x
+    };
+    println!("The number is now: {}", final_number)
 }
