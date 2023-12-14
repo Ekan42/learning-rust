22b967c54bb22378ed136ff9ef4e010600bba98e
Ekan42 <153546894+Ekan42@users.noreply.github.com>
2023-12-14 20:32:46 +0100

    Add another function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 fn times_two(number: i32) -> i32 {
     number * 2
 }
+
+fn divide_two(number: i32) -> i32 {
+    number / 2
+}
+
 fn main() {
     let final_number = {
-    let y = 10;
-    let x = 9; 
-    let x = times_two(x);
-    let x = x + y;
-    x
+        let y = 10;
+        let x = 9;
+        let x = times_two(x);
+        let x = x + y;
+        divide_two(x)
     };
     println!("The number is now: {}", final_number)
 }
+
